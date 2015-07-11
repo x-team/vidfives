1aa539119e65e52530eb921a81b63dd87974a65e
Josh Johnston <josh@x-team.com>
2015-07-11 12:44:43 +1000

    refactor tpl

--- src/page-tpl.js
+++ src/page-tpl.js
@@ -2,34 +2,35 @@
 
 var h = require('hyperscript');
 
+function stylesheet (href) {
+  return h('link', {
+    href: href,
+    rel: 'stylesheet',
+    type: 'text/css'
+  })
+}
+
+function script (src) {
+  return h('script', { src: src });
+}
+
 module.exports = function (contentElems) {
-  var fontElem = h('link', {
-    href: '//fonts.googleapis.com/css?family=Open+Sans:300italic,400,600,700,300',
-    rel: 'stylesheet',
-    type: 'text/css'
-  });
-
-  var fontAwesomeElem = h('link', {
-    href: '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
-    rel: 'stylesheet',
-    type: 'text/css'
-  });
-
   return h('html', [
     h('head', {}, [
       h('meta', { charset: 'UTF-8' }),
       h('title', '/vidfives'),
       h('meta', { name: 'viewport', content: 'width=device-width' }),
-      fontElem,
-      fontAwesomeElem,
-      h('link', { href: '/style.css', rel: 'stylesheet', type: 'text/css' }),
-      h('link', { href: '/dialog.css', rel: 'stylesheet', type: 'text/css' }),
-      h('link', { href: '/dialog-sandra.css', rel: 'stylesheet', type: 'text/css' }),
-      h('script', { src: '/js/modernizr.custom.js' })
+
+      stylesheet('//fonts.googleapis.com/css?family=Open+Sans:300italic,400,600,700,300'),
+      stylesheet('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'),
+      stylesheet('/style.css'),
+      stylesheet('/dialog.css'),
+      stylesheet('/dialog-sandra.css'),
+      script('/js/modernizr.custom.js')
     ]),
     h('body', {}, [
       h('#root', contentElems || []),
-      h('script', { src: '/main.js' })
+      script('/main.js')
     ])
   ]);
 };
